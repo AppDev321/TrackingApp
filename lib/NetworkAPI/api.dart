c41b2a80f8cb697b35b8bff15fd2cfee56cd82e1
Noman Mac <noman@thexsol.com>
2023-03-21 18:25:01 +0500

    new ip list

--- lib/NetworkAPI/api.dart
+++ lib/NetworkAPI/api.dart
@@ -2,6 +2,6 @@
 
 class AppURL {
   static var apiKey = "a92f28e11a27e8e5938a2020be68ba9c";
-  static var baseUrl= "http://192.168.18.50/fleet-management/api/";
+  static var baseUrl= "http://fleet.umair.ngsp.pk/api/";
   static var loginUrl = "${baseUrl}gateway/driverlogin";
 }
